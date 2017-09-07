07290a27baaca54c93d5530744e84aff793f4559
atec <root@atec.pub>
2017-09-07 13:37:49 -0400

    granular file-wise merge

--- common.go
+++ common.go
@@ -11,8 +11,6 @@ import (
 
 func execute(cmd *exec.Cmd) (string, error) {
 
-	pretty.Logln("[INFO] executing command", cmd.Path, cmd.Args)
-
 	var outbuf, errbuf bytes.Buffer
 	cmd.Stdout = &outbuf
 	cmd.Stderr = &errbuf
@@ -21,19 +19,30 @@ func execute(cmd *exec.Cmd) (string, error) {
 	stdout := outbuf.String()
 	stderr := errbuf.String()
 
-	pretty.Logln("[INFO] stdout:\n\n", stdout)
-	pretty.Logln("[INFO] stderr:\n\n", stderr)
-
-	if err != nil {
-
-		// special case for provisioning
-		if strings.Contains(stderr, "already exists") {
-			pretty.Logln("[INFO] looks like resource is already provisioned\n\n", stderr)
-			return stdout, resourceAlreadyProvisioned
-		}
-
-		return stdout, errors.New(stderr)
+	if *verbose {
+		pretty.Logln("[INFO] executing command", cmd.Path, cmd.Args, "...")
+		pretty.Logln("[INFO] stdout:\n\n", stdout)
+		pretty.Logln("[INFO] stderr:\n\n", stderr)
 	}
 
+	if err != nil {
+		return stdout, checkErr(stderr)
+	}
 	return stdout, nil
+}
+
+var (
+	portAlreadyAllocated       = errors.New("port is already allocated") // docker
+	resourceAlreadyProvisioned = errors.New("resource already exists")   // gce
+	tmpDirDoesntExist          = errors.New("tmp dir doesn't exist")     // unix fs
+)
+
+func checkErr(stderr string) error {
+	if strings.Contains(stderr, "port is already allocated") {
+		return portAlreadyAllocated
+	}
+	if strings.Contains(stderr, "already exists") {
+		return stdout, resourceAlreadyProvisioned
+	}
+	return errors.New(stderr)
 }
--- main.go
+++ main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"log"
-	"os/exec"
-	"path/filepath"
 
 	"github.com/kr/pretty"
 )
 
-const gitDir = "/home/git"
-
-var (
-	repoDir  = filepath.Join(gitDir, "www.git")
-	tmpDir   = filepath.Join(gitDir, "tmp")
-	buildDir = filepath.Join(tmpDir, "www")
-)
+var verbose = flag.Bool("verbose", false, "verbosity flag")
 
 type temp interface {
-	clone() error
 	build() error
 	test() error
 	provision() error
@@ -27,74 +18,18 @@ type temp interface {
 	monitor() error
 }
 
-var tmpDirDoesntExist = errors.New("tmp dir doesn't exist")
-
-func clean() error {
-	pretty.Logln("[INFO] cleaning...")
-	cmd := exec.Command("rm", "-rf", buildDir)
-	_, err := execute(cmd)
-	switch err {
-	case tmpDirDoesntExist:
-		pretty.Logln("[INFO] build dir doesn't exist. skipping")
-		return nil
-	default:
-		return err
-	}
-}
-
-func clone() error {
-	pretty.Logln("[INFO] cloning...")
-	cmd := exec.Command("git", "clone", repoDir, buildDir)
-	if _, err := execute(cmd); err != nil {
-		return err
-	}
-	return nil
-}
-
-func test() error {
-	pretty.Logln("[INFO] testing...")
-	return nil
-}
-
-var resourceAlreadyProvisioned = errors.New("resource already provisioned")
-
-func provision() error {
-	pretty.Logln("[INFO] provisioning...")
-	cmd := exec.Command("gcloud", "compute", "instances", "create", "atec", "--zone", "us-east1-b", "--tags", "http-server")
-	_, err := execute(cmd)
-	switch err {
-	case resourceAlreadyProvisioned:
-		pretty.Logln("[INFO] resource already provisioned. skipping")
-		return nil
-	default:
-		return err
-	}
-}
-
 func main() {
-	err := clean()
-	if err != nil {
-		pretty.Logln("[FATAL] failed to clean")
-		log.Fatal(err)
-	}
-	err = clone()
-	if err != nil {
-		pretty.Logln("[FATAL] failed to clone")
-		log.Fatal(err)
-	}
-	err = build()
+
+	flag.Parse()
+
+	ref, err := build()
 	if err != nil {
 		pretty.Logln("[FATAL] failed to build")
 		log.Fatal(err)
 	}
-	err = provision()
-	if err != nil {
-		pretty.Logln("[ERROR] failed to provision")
-		pretty.Logln(err)
-	}
-	err = configure()
-	if err != nil {
-		pretty.Logln("[FATAL] failed to configure")
+	if err := deploy(ref); err != nil {
+		pretty.Logln("[FATAL] failed to deploy")
 		log.Fatal(err)
 	}
+	pretty.Logln("[INFO] success!")
 }
