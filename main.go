c92fc6beb6e7f76266eaba73c141a602c65e5dbf
atec <root@atec.pub>
2017-09-14 15:58:10 -0400

    successfully rebuilds master when head changes

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"sync"
 
+	"atec.pub/sh"
 	"github.com/fsnotify/fsnotify"
 	"github.com/kr/pretty"
 )
@@ -17,6 +20,9 @@ type playground struct {
 	current []byte // last observed head
 	watcher *fsnotify.Watcher
 }
+
+// TODO
+const testPath = "/Users/atec/go/src/atec.pub/www"
 
 func (p playground) watch() {
 
@@ -33,27 +39,29 @@ func (p playground) watch() {
 	for {
 		select {
 		case event := <-p.watcher.Events:
-			pretty.Logln("event:", event)
-			if event.Op&fsnotify.Write == fsnotify.Write {
-				pretty.Logln("modified file:", event.Name)
-				next, err := ioutil.ReadFile(p.name)
-				if err != nil {
-					log.Fatal(err)
-				}
-				pretty.Logln("old head:", p.current)
-				pretty.Logln("new head:", next)
-				p.current = next
+
+			pretty.Logln("[INFO] rcvd event:", event)
+			pretty.Logln("[INFO] modified file:", event.Name)
+			next, err := ioutil.ReadFile(p.name)
+			if err != nil {
+				log.Fatal(err)
 			}
+			pretty.Logln("[INFO] old head:", strings.Trim(string(p.current), "\n"))
+			pretty.Logln("[INFO] new head:", strings.Trim(string(next), "\n"))
+			p.current = next
+
+			// TODO
+			sh.Run(exec.Command("rm", "-rf", testPath+"/.hermes/master"))
+			sh.Run(exec.Command("git", "clone", testPath, testPath+"/.hermes/master"))
+			sh.Run(exec.Command("bundle", "exec", "jekyll", "build", "-s", testPath+"/.hermes/master", "-d", testPath+"/.hermes/master/_site"))
+
 		case err := <-p.watcher.Errors:
-			pretty.Logln("error:", err)
+			pretty.Logln("[ERROR]", err)
 		}
 	}
 }
 
 func main() {
-
-	var wg sync.WaitGroup
-	wg.Add(1)
 
 	// read file info from directory
 	pretty.Logln("[INFO] reading directory...")
@@ -89,5 +97,9 @@ func main() {
 		go p.watch()
 
 	}
+
+	// block forever
+	var wg sync.WaitGroup
+	wg.Add(1)
 	wg.Wait()
 }
